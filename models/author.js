b58bf6dcaf54eafd4d30710b526ef3fff3e6350f
Des Barrett <des.barrett@gmail.com>
2018-08-09 12:53:36 +0100

    library working for display create delete

---
+++ models/author.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, max: 100},
+    family_name: {type: String, required: true, max: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+// virtual for date format
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  if(this.date_of_birth){
+  var formattedBirth = moment(this.date_of_birth).format('YYYY-MM-DD')
+  } else { var formattedBirth = ''}
+  if(this.date_of_death){
+  var formattedDeath = moment(this.date_of_death).format('YYYY-MM-DD')
+} else { var formattedDeath = ''}
+
+  return formattedBirth + ' - ' + formattedDeath;
+});
+
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
